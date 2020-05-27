6c737bd26a827cb45c3d663b8588ca17eddc872f
stavroskapris <stavroskapridakis@gmail.com>
2020-05-27 18:15:25 +0300

    to basic endpoints

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to work out of the day API homepage")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func wod(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		newWod(w, r)
+	} else {
+		_, err := fmt.Fprintf(w, "Invalid request method")
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func newWod(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "POST request to /wod endpoint!")
+	if err != nil {
+		log.Panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("The project is setup correctly!")
+	http.HandleFunc("/", home)
+	http.HandleFunc("/work-out-of-the-day", wod)
+	fmt.Printf("Server listening on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
